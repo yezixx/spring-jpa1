5c9bcad1340a433057479dfe42bc6292ccaf7ada
yezixx <skylow917@daum.net>
2024-12-26 20:53:58 +0900

    회원 등록 API

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+// @Controller @ResponseBody -> @RestController
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        // @RequestBody -> JSON으로 온 body를 Member에 그대로 매핑해서 넣어줌
+        // @Valid -> 해당 엔티티에 @NotEmpty 같은 제약 조건 걸어둔 거 validation
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member();
+        member.setName(request.getName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse{
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data @Getter @Setter
+    static class CreateMemberRequest {
+        private String name;
+    }
+}
